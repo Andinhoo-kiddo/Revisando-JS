f8d7709f955c5540dad21e33aee3a5e1183367ee
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-08-03 20:27:41 -0300

    Adicionando - objetoRevisao

---
+++ objetos/objetoRevisao.js
@@ -0,0 +1,39 @@
+// coleção dinamica de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca do produto'] = 'Generica'
+produto.preco = 222
+
+console.log(produto)
+
+delete produto['marca do produto']
+delete produto.preco 
+console.log(produto)
+
+const carro = {
+    modelo: 'Skyline GT-R',
+    preco: 6700000,
+        proprietario: {
+            nome: 'Andinho rei delas',
+            idade: 17,
+            endereco: {
+                logradouro: 'Rua ABA',
+                numero: 22
+            }
+        },
+        condutores: [
+            {
+                nome: 'Junior',
+                idade: 19
+        }, {
+            nome: 'Ana',
+            idade: 26
+        }], 
+        calcularValorSeguro: function() {
+            // ...
+        }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Estádio São Januário' // qnd vc receber de algum parametro string, pode acessar assim
+console.log(carro)
