c086953958f609f744f4501a2ed58683e76d7e97
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-08-02 23:05:29 -0300

    Modificando - Orientação à Objetos Revisão

--- objetos/oo.js
+++ objetos/oo.js
@@ -1,1 +1,23 @@
-console.log('d')
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural 
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()  // o Foco passou a ser o objeto
+
+//Principios importantes: 
+// 1. abstração
+// 2. encapsulamento 
+// 3. herança (prototype) -- priorize a composição!(TEM um ao invés do É um)
+// 4. polimorfismo 
